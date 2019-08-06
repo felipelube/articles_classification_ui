ad44c75f0d21139c9f332245e71765f409d1c455
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-08-06 18:51:10 -0300

    chore: habilite os ícones do pacote materialdesignicons

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
+import '@mdi/font/css/materialdesignicons.css'
+
 
 Vue.config.productionTip = false
 Vue.use(Buefy)
