585a7c3b30e796c8c7d2379cdcc88d19abfbdb82
Felipe Lube de Bragança <felipe.lubra@gmail.com>
2019-08-22 10:47:03 -0300

    feat: defina o tipo de dado esperado para cada campo

--- src/config/index.js
+++ src/config/index.js
@@ -9,45 +9,53 @@ export const REQUIREMENT_FIELDS_INFO = [
   {
     name: 'titleLangEnglish',
     title: 'Título em inglês?',
+    type: 'boolean',
     component: 'b-checkbox',
 
   },
   {
     name: 'titlePresence',
     title: 'Título presente no PDF?',
+    type: 'boolean',
     component: 'b-checkbox',
 
   },
   {
     name: 'abstractLangEnglish',
     title: 'Abstract em inglês?',
+    type: 'boolean',
     component: 'b-checkbox',
 
   },
   {
     name: 'abstractPresence',
     title: 'Abstract presente no PDF?',
+    type: 'boolean',
     component: 'b-checkbox',
 
   },
   {
     name: 'authorsPresence',
     title: 'Autores presentes no PDF?',
+    type: 'boolean',
     component: 'b-checkbox'
   },
   {
     name: 'originalTitlePresence',
     title: 'Titulo original presente no PDF?',
+    type: 'boolean',
     component: 'b-checkbox'
   },
   {
     name: 'referencesInRomanScript',
     title: 'Todas referências no alfabeto romano?',
+    type: 'boolean',
     component: 'b-checkbox'
   },
   {
     name: 'lang',
     title: 'Linguagem do artigo',
+    type: 'string',
     component: 'LanguageField',
   },
 ]
